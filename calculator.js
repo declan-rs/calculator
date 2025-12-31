4fba0d22b17dbcc72e3e9e56eedfe83f33b89a0f
declan-rs <declan.rsg@gmail.com>
2025-12-31 15:30:31 +1100

    Add function that operates given two numbers and operator

--- calculator.js
+++ calculator.js
@@ -13,3 +13,18 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+// Takes operator and two numbers, returns result
+function operate(op, x, y) {
+    if(op == '+') {
+        return add(x, y);
+    } else if(op == '-') {
+        return subtract(x, y);
+    } else if(op == '*') {
+        return multiply(x, y); 
+    } else if(op == '/') {
+        return divide(x, y);
+    } else {
+        return "Error, invalid operator";
+    }
+}
