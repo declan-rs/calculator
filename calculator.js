9becf8263d2e96a4091a087b20084a887cf4714b
declan-rs <declan.rsg@gmail.com>
2026-01-06 14:52:26 +1100

    User numbers are saved and updated on display

--- calculator.js
+++ calculator.js
@@ -1,5 +1,10 @@
-currentNum = 0;
-displayNumber(currentNum);
+num1 = "";
+num2 = "";
+operator = "";
+updateDisplay();
+hasNum1 = false;
+hasOp = false;
+ready = false;
 
 function add(x, y) {
     return x + y;
@@ -18,7 +23,7 @@ function divide(x, y) {
 }
 
 // Takes operator and two numbers, returns result
-function operate(op, x, y) {
+function operate(x, op, y) {
     if(op == '+') {
         return add(x, y);
     } else if(op == '-') {
@@ -32,19 +37,45 @@ function operate(op, x, y) {
     }
 }
 
-// Puts another number on the display
-function displayNumber(num) {
-    document.getElementById("display").textContent = num;
+function equals() {
+    if(ready == false) {
+        // Not a complete expression
+        return;
+    }
+    result = operate(num1, operator, num2);
+    num1 = result;
+    operator = "";
+    num2 = 0;
+    updateDisplay(result);
+    hasOp = false;
+    ready = false;
+}
+
+function numClicked(num) {
+    if(hasNum1 == false) {
+        num1 = num;
+        hasNum1 = true;
+    } else if(hasOp == false) {
+        num1 += num;
+    }
+
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById("display").textContent = num1 + operator + num2;
 }
 
 // Add button event listeners
-document.getElementById("0").addEventListener("click", () => displayNumber(0));
-document.getElementById("1").addEventListener("click", () => displayNumber(1));
-document.getElementById("2").addEventListener("click", () => displayNumber(2));
-document.getElementById("3").addEventListener("click", () => displayNumber(3));
-document.getElementById("4").addEventListener("click", () => displayNumber(4));
-document.getElementById("5").addEventListener("click", () => displayNumber(5));
-document.getElementById("6").addEventListener("click", () => displayNumber(6));
-document.getElementById("7").addEventListener("click", () => displayNumber(7));
-document.getElementById("8").addEventListener("click", () => displayNumber(8));
-document.getElementById("9").addEventListener("click", () => displayNumber(9));
+document.getElementById("0").addEventListener("click", () => numClicked("0"));
+document.getElementById("1").addEventListener("click", () => numClicked("1"));
+document.getElementById("2").addEventListener("click", () => numClicked("2"));
+document.getElementById("3").addEventListener("click", () => numClicked("3"));
+document.getElementById("4").addEventListener("click", () => numClicked("4"));
+document.getElementById("5").addEventListener("click", () => numClicked("5"));
+document.getElementById("6").addEventListener("click", () => numClicked("6"));
+document.getElementById("7").addEventListener("click", () => numClicked("7"));
+document.getElementById("8").addEventListener("click", () => numClicked("8"));
+document.getElementById("9").addEventListener("click", () => numClicked("9"));
+
+document.getElementById("=").addEventListener("click", () => equals());
